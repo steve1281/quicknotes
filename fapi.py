d07910f285e117b8fe121b042703c93cf9b436b9
Steve Falcigno <steve1281@hotmail.com>
2021-04-03 18:48:45 -0400

    minor cleanups

--- fapi.py
+++ fapi.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 import uvicorn
 import re
@@ -36,7 +35,6 @@ md_extensions = [
 initfolder = os.getenv('QUICKNOTES', '/docs/')
 if not initfolder.endswith('/'):
     initfolder = initfolder + "/"
-
 _port = os.getenv('PORT', '8000')
 _ip = os.getenv('IPADDRESS', '127.0.0.1')
 
@@ -65,11 +63,18 @@ def build_response(body):
     return template.replace("[STYLE]", get_style()).replace("[BODY]", body)
 
 
+def build_quicknotes():
+    files = list_files(initfolder)
+    quicknotes = sorted(quicknotelist(files), key=lambda x: int(re.split("-| ", x)[0]))
+    return quicknotes
+
+
 # --- API ---
 @app.get("/", response_class=HTMLResponse)
 async def root():
     template = template_loader(initfolder+"templates/root.template")
-    return build_response(template.replace("[IPADDRESS]", _ip).replace("[PORT]", _port).replace("[INITFOLDER]", initfolder))
+    return build_response(template.replace("[IPADDRESS]", _ip).replace("[PORT]", _port)
+                          .replace("[INITFOLDER]", initfolder))
 
 
 @app.get("/blah/blah", response_class=HTMLResponse)
@@ -79,38 +84,37 @@ async def blah():
 
 @app.get("/list", response_class=HTMLResponse)
 async def list_quick_notes():
-    files = list_files(initfolder)
-    quicknotes = sorted(quicknotelist(files), key=lambda x: int(re.split("-| ", x)[0]))
-    add_list_converter = ('<li><a class="quickanchor" href="' + w + '">' + w + '</a></li>' for w in quicknotes)
+    quicknotes = build_quicknotes()
+    add_list_converter = ('<li><a class="quickanchor" href="' + w + '">'
+                          + w + '</a></li>' for w in quicknotes)
     body = "<ul id='quicklist'>" + "\n".join(add_list_converter) + "</ul>"
     return build_response(body)
 
 
 @app.get("/filter={filters}", response_class=HTMLResponse)
 async def read_item(filters):
-    files = list_files(initfolder)
-    quicknotes = sorted(quicknotelist(files), key=lambda x: int(re.split("-| ", x)[0]))
+    quicknotes = build_quicknotes()
     filter_list = filters.split(",")
     filter_list.reverse()
     filtered_list = quicknotes
     for s in filter_list:
         filtered_list = filterout(initfolder, filtered_list, s)
-    add_list_converter = ('<li><a class="quickanchor" href="http://' + _ip + ':' + _port + '/' + w + '">' + w + '</a></li>' for w in filtered_list)
+    add_list_converter = ('<li><a class="quickanchor" href="http://' + _ip + ':' + _port + '/' + w + '">'
+                          + w + '</a></li>' for w in filtered_list)
     body = "<ul>" + "\n".join(add_list_converter) + "</ul>"
     return build_response(body)
 
 
 @app.get("/templates/{resource}")
 async def templates_folder(resource: str):
-    return HTMLResponse(build_response("No access."))
+    return HTMLResponse(build_response(f"No access to {resource}."))
 
 
 @app.get('/{filename}', include_in_schema=False)
 async def all_others(filename: str):
     _, ext = os.path.splitext(filename)
-    if ext.lower() in ['.jpg', '.gif', '.png', '.jpeg','.mp4']:
-        with open(initfolder + filename, "rb") as f:
-            return FileResponse(path=initfolder+filename)
+    if ext.lower() in ['.jpg', '.gif', '.png', '.jpeg', '.mp4']:
+        return FileResponse(path=initfolder+filename)
     elif ext in ['.md', '.MD']:
         s, _ = dumpQuickNote(initfolder, strip(filename))
         s = markdown.markdown(s, extensions=md_extensions)
