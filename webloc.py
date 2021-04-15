0f92afa03712936ecd2dd1ccfcfa66bc71b04648
Steve Falcigno <steve1281@hotmail.com>
2021-04-14 22:09:09 -0400

    add support for .url and .desktop to webloc.py

--- webloc.py
+++ webloc.py
@@ -1,5 +1,6 @@
 import os
 from bs4 import BeautifulSoup
+from  configparser import ConfigParser
 
 
 def get_webloc_files(document_folder, webloc_folder):
@@ -10,22 +11,61 @@ def get_webloc_files(document_folder, webloc_folder):
             full_name = os.path.join(path, name)
             if os.path.isfile(full_name):
                 _, ext = os.path.splitext(name)
-                if ext.lower() == '.webloc':
+                if ext.lower() in ['.webloc', '.url', '.desktop']:
                     files.append(full_name)
     except FileNotFoundError:
         files = None
     return files
 
 
+def webloc_get(filename):
+    """
+    Scan a .webloc file (mac) for a URL
+    :param filename:
+    :return:
+    """
+    with open(filename, 'r') as f:
+        data = f.read()
+    bs_data = BeautifulSoup(data, 'xml')
+    return bs_data.find('string').text
+
+
+def desktop_get(filename):
+    """
+    Scan a .desktop file (ubuntu) for a URL
+    :param filename:
+    :return:
+    """
+    config = ConfigParser()
+    config.read(filename)
+    return config['Desktop Entry']['URL']
+
+
+def url_get(filename):
+    """
+    Scan a .url file  (windows) for a URL
+    :param filename:
+    :return:
+    """
+    config = ConfigParser()
+    config.read(filename)
+    return config['InternetShortcut']['URL']
+
+
 def build_url_list(files):
     url_list = dict()
     for filename in files:
         try:
-            with open(filename, 'r') as f:
-                data = f.read()
-            bs_data = BeautifulSoup(data, 'xml')
-            url = bs_data.find('string').text
-            url_list[os.path.splitext(os.path.basename(filename))[0]] = url
+            display_name, ext = os.path.splitext(os.path.basename(filename))
+            if ext.lower() == '.webloc':
+                url_list[display_name] = webloc_get(filename)
+            elif ext.lower() == '.url':
+                url_list[display_name] = url_get(filename)
+            elif ext.lower() == '.desktop':
+                url_list[display_name] = desktop_get(filename)
+            else:
+                pass
+
         except FileNotFoundError:
             pass
     return url_list
